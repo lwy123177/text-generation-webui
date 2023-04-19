3dc33149167820dc2b321edb8c60bdbb37cafbb2
lwy123177 <118701490+lwy123177@users.noreply.github.com>
2023-04-18 20:55:16 -0400

    Add do_OPTIONS

--- extensions/api/script.py
+++ extensions/api/script.py
@@ -11,6 +11,13 @@ params = {
 
 
 class Handler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
+        self.send_header('Access-Control-Max-Age', '86400')
+        self.end_headers()
     def do_GET(self):
         if self.path == '/api/v1/model':
             self.send_response(200)
